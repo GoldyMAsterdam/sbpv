aed35617f51f45bd34580d62d4f61799db2eb17b
rafisverycool <rafivanderlaan@gmail.com>
2026-03-12 00:42:25 +0100

    fixed merge issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import Pv from './pv';
+import Home from './home';
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pv" element={<Pv />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-        <h1>HI</h1>
-    </div>
   );
 }
